efd309c86066c67588d42cedc95cb6454da78666
Shubham <shubhamchauhan9@gmail.com>
2023-06-25 00:59:19 +0530

    sign up sign in user profile code base created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,33 @@
 module.exports.profile = function(req, res) {
-    res.end('<h1>User Profile</h1>');
+    // res.end('<h1>User Profile</h1>');
+    return res.render('users', {
+        title: "User"
+    })
+}
+
+
+// render sign in page
+module.exports.signUp = function(req, res) {
+
+    return res.render('user_sign_up', {
+        title: "codeial | user sign up"
+    })
+}
+
+
+// render sign up page
+module.exports.signIn = function(req, res) {
+    return res.render('user_sign_in', {
+        title: "codeial | user sign in"
+    })
+}
+
+// get the sign up for the user
+module.exports.create = function(req, res) {
+    // todo later
+}
+
+// sign in and create a session for the user
+module.exports.create_session = function(req, res) {
+    // todo later
 }
--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const app = require('express')();
-
+const cookieParser = require('cookie-parser');
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 const port = 8000;
 
 // use express router
 app.use('/', require('./routes'));
 
+app.use(require('express').urlencoded());
+
+app.use(require('express').static('./assets'));
+
+app.use(cookieParser());
+
+app.use(expressLayouts);
+
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 // setting up our view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 
+// starting our server on port:8000
 app.listen(port, function(err) {
     if (err) {
         console.log(`Error in running the server:${err}`);
