5a464b477884a2383f3ed6a55b56be40ea4b5778
Shubham <shubhamchauhan9@gmail.com>
2023-07-17 13:37:31 +0530

    home css added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -13,7 +13,7 @@ module.exports.home = async function(req, res) {
         }).exec();
         let foundUsers = await User.find({});
         return res.render('home', {
-            title: "Codeial | Home",
+            title: "Home",
             posts: foundPosts,
             all_users: foundUsers
         })
